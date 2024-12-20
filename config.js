0d3f0379c92ff15a41a21a21b767f5a9953252a1
Raveeshab4 <109133252+raveeshad5@users.noreply.github.com>
2024-12-20 14:05:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "z3p3EZDZ#Ram1FZbK3Kd2FqWNK7GCqps-7crYqMLYMh_XiAl5HoY",/ Add your session id
+SESSION_ID: process.env.SESSION_ID || "LroDiaBK#7zJNy7xp9AYTU7wbkyFNhvQY_ch_R94TG85hFOpJZ1A",/ Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
